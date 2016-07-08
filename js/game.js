c440b1a790d3484f8410590ea32f96278b662716
Ingrid Cronburg <ingrid257@gmail.com>
2016-07-07 21:22:53 -0400

    refactoring

--- js/game.js
+++ js/game.js
@@ -7,7 +7,12 @@ GuessingGame.prototype.reset = function() {
   this.winningNumber = Math.floor(Math.random() * 100) + 1  ;
   this.guess = null;
   this.guesses = [];
+  this.hint = [Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1, this.winningNumber].sort(function(x, y) { return x - y }).join(", ");
 };
+
+GuessingGame.prototype.getWinningNumber = function() {
+  return this.winningNumber;
+}
 
 GuessingGame.prototype.setGuess = function(guess) {
   this.guess = parseInt(guess);
@@ -43,10 +48,14 @@ GuessingGame.prototype.lowerOrHigher = function() {
   }
 };
 
+GuessingGame.prototype.isWarm = function() {
+  return Math.abs(this.guess - this.winningNumber) < 20;
+};
+
 GuessingGame.prototype.canPlay = function() {
   return (this.guessCount() + 1) < this.maxGuesses;
 };
 
-GuessingGame.prototype.hint = function() {
-  return [Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1, this.winningNumber];
+GuessingGame.prototype.getHint = function() {
+  return this.hint;
 };
